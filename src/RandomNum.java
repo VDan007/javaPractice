aefa24b4f07cddc57866eda764be8a198133fe2a
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-25 02:20:42 +0200

    random

---
+++ src/RandomNum.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomNum {
+    public static void main(String[] args) throws Exception {
+        Random random = new Random();
+        int x = random.nextInt(6)+1;
+        double y = random.nextDouble();
+        boolean z = random.nextBoolean();
+        System.out.println(x + " " + y + " " +z );
+    }
+}
