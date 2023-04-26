db20f84b6b7e62dd0af89f3ae0dbb3de7edb113d
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-26 12:53:47 +0200

    nestedLoops

---
+++ src/NestedLoops.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NestedLoops {
+    public static void main(){
+        Scanner scn = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol = "";
+
+        System.out.println("enter number of rows");
+        rows= scn.nextInt();
+        System.out.println("enter number of columns");
+        columns = scn.nextInt();
+        System.out.println("enter symbol to use");
+        symbol = scn.next();
+
+        for(int i =1; i<=rows; i++){
+            System.out.println();
+            for(int j=1; j<=columns;j++){
+                System.out.print(symbol);
+            }
+        }
+        scn.close();
+
+    }
+}
