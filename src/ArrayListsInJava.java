d0a1dba1222f0c8508032c59e5448f2168e2c1cc
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-27 19:25:19 +0200

    f_o_t_d

---
+++ src/ArrayListsInJava.java
@@ -0,0 +1,9 @@
+public class ArrayListsInJava {
+    public static void main(String[] args){
+        //Arraylists = a resizible array;
+                    //    Elements can be added and removed after compilation phase. 
+                    //    store reference data types
+
+      //  ArrayList<String >        
+    }
+}
