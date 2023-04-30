ec010d0db1783433ed33ab777202e93ea83690f6
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-30 22:54:50 +0200

    copy objects

--- src/Car.java
+++ src/Car.java
@@ -1,19 +1,43 @@
 public class Car extends Vehicle {
-    String make = "Ford";
-    String model = "Mustang";
-    int year = 2023;
-    String color = "red";
-    double price = 500.00;
-    
-
+    String make;
+    String model;
+    int year;
+    double price;
+    String color;
     String name;
 
     Car(String name){
         this.name = name;
     }
 
+    Car(String make,String model,int year){
+        setMake(make);
+        setModel(model);
+        setYear(year);
+    }
     
-    
+
+    public String getMake(){
+        return this.make;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setMake(String make){
+        this.make = make;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+
+
 
     void drive(){
         System.out.println("gooo");
--- src/Plane.java
+++ src/Plane.java
@@ -4,9 +4,9 @@ public class Plane {
     private int year;
 
     Plane(String make,String model,int year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
+        setMake(make);
+        setModel(model);
+        setYear(year);
     }
 
     public String getMake(){
@@ -18,8 +18,14 @@ public class Plane {
     public int getYear(){
         return year;
     }
-    public void setYear(int newYear){
-        this.year = newYear;
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public void setMake(String make){
+        this.make = make;
     }
 
 }
