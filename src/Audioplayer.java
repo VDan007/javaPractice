7e54e2ae662a846b1dad7c991758acdad5798bbc
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-03 14:57:27 +0200

    player works

--- src/Audioplayer.java
+++ src/Audioplayer.java
@@ -14,7 +14,29 @@ public class Audioplayer {
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
         
-        clip.start();
-        String response = scanner.next();
+        String response = "";
+        
+        while( !response.equals("Q") ){
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.print("Enter your choice");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch(response){
+                case ("P") : clip.start();
+                break;
+                case ("S") : clip.stop();
+                break;
+                case ("R") : clip.setMicrosecondPosition(0);
+                break;
+                case("Q") : clip.close();
+                break;
+                default: System.out.println("Not a valid response "); 
+                
+            }
+        }
+
+      
     }
 }    
