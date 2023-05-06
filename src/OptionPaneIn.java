b8cc0dfe90a28c3e082c6717e289cb3367c93456
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-06 13:04:20 +0200

    u c f y

--- src/OptionPaneIn.java
+++ src/OptionPaneIn.java
@@ -1,3 +1,5 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class OptionPaneIn {
@@ -8,6 +10,23 @@ public class OptionPaneIn {
      //   JOptionPane .showMessageDialog(null, args, "message", JOptionPane.INFORMATION_MESSAGE);
     //   JOptionPane .showMessageDialog(null, "you has a virus?", "message", JOptionPane.WARNING_MESSAGE);
       //  JOptionPane .showMessageDialog(null, "call support now", "message", JOptionPane.ERROR_MESSAGE);
-      JOptionPane.showConfirmDialog(null, "do you even code", "Choose", JOptionPane.YES_NO_CANCEL_OPTION);
+     // JOptionPane.showConfirmDialog(null, "do you even code", "Choose", JOptionPane.YES_NO_CANCEL_OPTION);
+
+    //  int userAnswer = JOptionPane.showConfirmDialog(null, "Bro dou you even code?", "answer",JOptionPane.YES_NO_CANCEL_OPTION);
+    //  System.out.println(userAnswer);
+     
+
+    // String name = JOptionPane.showInputDialog("What is your name?");
+
+    // System.out.println(name + (userAnswer == 0 ? " does code" : " doesn't code") );
+
+      String[] responses= {"You are awesome","Thank you","blush"};
+
+      ImageIcon image = new ImageIcon("fire.png");
+      JOptionPane.showOptionDialog(null, "you are awesome",
+                                   "secret message", JOptionPane.YES_NO_CANCEL_OPTION, 
+                                   JOptionPane.INFORMATION_MESSAGE, image , responses, 
+                                   0);
+
     }
 }
