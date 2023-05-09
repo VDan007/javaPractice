5c86b35d9bab9299b8c92b9814fc69d117dcfdc3
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-09 23:11:19 +0200

    mouse listener bug

--- src/MouseListenerFrame.java
+++ src/MouseListenerFrame.java
@@ -1,12 +1,72 @@
-import javax.swing.JFrame;
 
-public class MouseListenerFrame extends JFrame {
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+
+public class MouseListenerFrame extends JFrame implements MouseListener {
+
+    JLabel label;
     
     MouseListenerFrame(){
+
+        
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(0,0,100,100);
+       // label.setSize(300, 300);
+        label.setBackground(Color.RED);
+        label.setOpaque(true);
+        label.addMouseListener(this);
+
+        this.add(label);
         this.setVisible(true);
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("clicked");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+
+
+    
+    
+  
+
+  
+    
 }
